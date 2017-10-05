084ad34726512f0c6de1f2c57a33db853c77e895
Sigureya <sigureya1208@gmail.com>
2017-10-05 17:39:14 +0900

    アイコン描画機能を復活

--- Mano_EnemyHelpWindow.js
+++ Mano_EnemyHelpWindow.js
@@ -9,7 +9,7 @@
 // [github]:https://github.com/Sigureya/RPGmakerMV
 //=============================================================================
 
-/*:ja
+/*:
  * @plugindesc 攻撃対象を選ぶ段階で相手の弱点を表示します。
  * 
  * @author しぐれん（魔のささやき）
@@ -70,14 +70,14 @@
  * @param displayMode
  * @type select
  * @option text
- * @_option icon
+ * @option icon
  * @desc 属性を文字で表示するかアイコンで表示するかを決めます。
  * @default text
  * 
  * @param iconList
- * @type number[]
- * @desc アイコンの一覧を定義します。
- * 属性の並び順と同じように、並べてください。
+ * @type []
+ * @desc アイコンを属性の並び順と同じように、並べてください。
+ * テキスト欄を右クリックすることで、アイコン選択画面が出ます。
  * @default ["64","65", "66", "67", "68", "69", "70", "71"]
  * 
  *
@@ -94,15 +94,26 @@
     'use strict';
 
     function toIcon(elementId){
-        return '\\['+enemyHelp.elementIcon[elementId]+']';
+        return '\\['+setting.elementIcon[elementId]+']';
     }
     function toElementName(elementId){        
         return $dataSystem.elements[elementId];
     }
+    /**
+     * @return {number[]}
+     */
+    function createIconLiset(param){
+        const px = JSON.parse(param.iconList);
+        const result=px.map(function(v){
+            return Number(v);
+        });
+        result.unshift(null);
+        return result;
+    }
 
 
 	var param = PluginManager.parameters('Mano_EnemyHelpWindow');
-	var enemyHelp ={
+	const setting ={
         weakLine:Number(param.WeakLine)/100,
 	    resistanceLine:Number(param.ResistanceLine)/100,
         weakName : String(param.WeakName),
@@ -114,67 +125,118 @@
             width:Number(param.WindowWidth),
             height:Number(param.WindowHeight)
         },
-        elementIcon:param.iconList,
+        elementIcon:createIconLiset( param),
       };
-    const elementItemFunc=enemyHelp.displayMode==='text' ? toElementName:toIcon;
+    const elementItemFunc=setting.displayMode==='text' ? toElementName:toIcon;
 
-class Window_EenmyHelp extends Window_Base{
+class Window_EenmyHelp extends Window_Selectable{
     constructor(x,y,w,h){
-        super(x,y,w,h);
+        super(x,y,240,120);
+        
+        this.move(x,y,w,this.fittingHeight(2));
         this._textCache =[];
+        this._infoX = Math.max(this.textWidth( setting.weakName) ,this.textWidth(setting.resistanceName));
+        this._batller=null;
     }
+    /**
+     * @return {Game_Battler}
+     */
+    battler(){
+        return this._batller;
+    }
+    windowWidth(){
+        return 240;
+    }
+    windowHeight(){
+        return this.fittingHeight(2);
+    }
+    /**
+     * @param {Game_Battler} battler 
+     */
+    createWeakList(battler){
+        const len = $dataSystem.elements.length;
+        const weakList=[];
+        const resistanceList=[];
+        for(var i=1; i< len;i+=1){
+            const rate =battler.elementRate(i);
+            if(rate> setting.weakLine){
+                weakList.push(i);
+            }else  if(rate < setting.resistanceLine){
+                resistanceList.push(i);
+            }
+        }
+        return {
+            weakList:weakList,
+            resistanceList:resistanceList
+        };
+    }
+
     clearCache(){
         this._textCache.length=0;
     }
-    createEnemyHelpText(battler){
-        const len = $dataSystem.elements.length;
-        var weakList=[];
-        var resistanceList=[];
-        
-        for(var i=1; i< len;i+=1){
-            const rate =battler.elementRate(i);
-            if(rate> enemyHelp.weakLine){
-                weakList.push(i);
-            }else  if(rate < enemyHelp.resistanceLine){
-            resistanceList.push(i);
-            }
+
+    drawBaseText(){
+        this.drawText(setting.weakName,0,0);
+        this.drawText(setting.resistanceName,0,this.lineHeight());
+    }
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number[]} elementList 
+     */
+    drawIcons(x,y,elementList){
+        for(var i =0 ;i<elementList.length;++i){
+            var iconId = setting.elementIcon[elementList[i]];
+            this.drawIcon(  iconId, x + Window_Base._iconWidth *i,y);
         }
-        var weakText = '\n'+(enemyHelp.weakName);
-        weakList.forEach(function(value){
-        weakText += elementItemFunc(value);
-        });
-        var resistanceText ='\n' +enemyHelp.resistanceName;
-        resistanceList.forEach(function(value){
-        resistanceText += elementItemFunc(value);
-        });
-    
-        return battler.name()+weakText + resistanceText;
+    }
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number[]} elementList 
+     */
+    drawElementText(x,y,elementList){
+        var str ='';
+        for(var i =0 ;i<elementList.length;++i){
+            var index = elementList[i];
+            str += $dataSystem.elements[index];
+        }
+        this.drawText(str,x,y);
     }
 
-    setItem(enemyBattler){
-        const enemy = enemyBattler.enemy();
-        const index =enemyBattler.index();
-        if(!this._textCache[index]){
-            this._textCache[index]=this.createEnemyHelpText(enemyBattler);
+    drawItem(){
+        const list = this.createWeakList(this.battler());
+        
+        if(setting.displayMode==='icon'){
+            this.drawIcons(this._infoX,0,list.weakList);
+            this.drawIcons(this._infoX,this.lineHeight(),list.resistanceList);
+        }else{
+            this.drawElementText(this._infoX,0,list.weakList);
+            this.drawElementText(this._infoX,this.lineHeight(),list.resistanceList);            
         }
-
+    }
+    /**
+     * @param {Game_Battler} enemyBattler 
+     */
+    setBattler(enemyBattler){
+        this._batller =enemyBattler;
         this.contents.clear();
-        this.drawTextEx(this._textCache[index],0,0);
-
+        this.drawBaseText();
+        this.drawItem();
     }
 };
 	
 const Scene_Battle_createAllWindows = Scene_Battle.prototype.createAllWindows;
 Scene_Battle.prototype.createAllWindows=function(){
     Scene_Battle_createAllWindows.call(this);
-    const r = enemyHelp.rect;
+    const r = setting.rect;
     const x = !Number.isNaN(r.x) ? r.x : Graphics.boxWidth- r.width;
     const y = !Number.isNaN(r.y) ? r.y : this._enemyWindow.y - r.height;
     var help =new Window_EenmyHelp(x,y,r.width,r.height);
     this.addChild(help);
     help.hide();
     this._enemyHelpWindow_MA =help;
-    this._enemyWindow.setHelpWindow(help);
+    this._enemyWindow.setEnemyHelpWindow(help);
 };
 
 Scene_Battle.prototype.isInputCompleted =function(){
@@ -191,22 +253,40 @@ Scene_Battle.prototype.selectNextCommand = function() {
         this.onInputComplete();
     }
 };
+/**
+ * @param {Window_EenmyHelp} help
+ */
+Window_BattleEnemy.prototype.setEnemyHelpWindow=function(help){
+    const w = help.windowWidth();
+    const h = help.windowHeight();
+    const x =this.x + this.windowWidth()-w;
+    const y = this.y -h;
+    help.move(x,y,w,h);
+    
+    this._enemyHelpWindow_MA =help;
 
+};
 
 const Window_BattleEnemy_show = Window_BattleEnemy.prototype.show;
 Window_BattleEnemy.prototype.show = function() {
     Window_BattleEnemy_show.call(this);
-    this.showHelpWindow();
+    this._enemyHelpWindow_MA.show();
 };
 
 const Window_BattleEnemy_hide = Window_BattleEnemy.prototype.hide;
 Window_BattleEnemy.prototype.hide = function() {
     Window_BattleEnemy_hide.call(this);
-    this.hideHelpWindow();
+    if(this._enemyHelpWindow_MA){
+        this._enemyHelpWindow_MA.hide();
+    }
 };
 
-Window_BattleEnemy.prototype.updateHelp = function() {
-    this.setHelpWindowItem(this.enemy());
+const Window_BattleEnemy_callUpdateHelp=Window_BattleEnemy.prototype.callUpdateHelp;
+Window_BattleEnemy.prototype.callUpdateHelp = function() {
+    Window_BattleEnemy_callUpdateHelp.call(this);
+    if(this._enemyHelpWindow_MA){
+        this._enemyHelpWindow_MA.setBattler(this.enemy());
+    }
 };
 
 })();
